4720011a01c40396016f74d0279e57266e85bdf0
Kirill <mr.turanov98@gmail.com>
2023-09-13 20:46:07 +0300

    I completed my first big project, it was a big one for me

--- app.js
+++ app.js
@@ -1,10 +1,41 @@
 let result = 0;
+let playerScore = 0;
+let computerScore = 0;
+let gameOver = false;
 let rock = document.getElementById('rock');
 let paper = document.getElementById('paper');
 let scissors = document.getElementById('scissors');
 let userChoiceElement = document.querySelector('#user-choice');
 let computerChoiceElement = document.querySelector('#computer-choice');
 let resultsElement = document.querySelector('#result-message');
+
+let playerScoreElement = document.querySelector('#player-score')
+let computerScoreElement = document.querySelector('#computer-score')
+
+
+function updateScore () {
+    playerScoreElement.textContent = `Your score: ${playerScore}`
+    computerScoreElement.textContent = `Computer score: ${computerScore}`
+}
+
+function checkGameOver () {
+    if(playerScore === 5) {
+        gameOver = true;
+        resultsElement.textContent = 'Game over. You win!'
+        showEndgameModal('Game over. You Win!')
+    }
+    if(computerScore === 5) {
+        gameOver = true;
+        resultsElement.textContent = 'Game over. You lose!'
+        showEndgameModal('Game over. You lose!')
+    }
+
+    if(gameOver) {
+        rock.style.display = none;
+        paper.style.display = none;
+        scissors.style.display = none;
+    }
+}
 
 function getComputerChoice () {
     const array = ['rock','paper','scissors']
@@ -18,17 +49,59 @@ function theWinner(player,computer) {
     if( player === 'rock' && computer === 'scissors' ||
         player === 'paper' && computer === 'rock'    ||
         player === 'scissors' && computer === 'paper') {
-            return 'You Win!'
+        playerScore ++;
+        updateScore();
+        checkGameOver();
+        return 'You Win!'
         }
+        computerScore++;
+        updateScore()
+        checkGameOver()
         return 'You Lose!'
 }
 function game(player) {
-    userChoiceElement.textContent = `You choice ${player}`
+    if(gameOver) {
+        return;
+    }
+    userChoiceElement.textContent = `You choice: ${player}`
     const computerChoice = getComputerChoice();
-    computerChoiceElement.textContent = `Computer choice ${computerChoice}`
+    computerChoiceElement.textContent = `Computer choice: ${computerChoice}`
     result = theWinner(player,computerChoice)
     resultsElement.textContent = `Result: ${result}`
 }
 rock.addEventListener('click',() => game('rock'))
 paper.addEventListener('click',() => game('paper'))
 scissors.addEventListener('click',() => game('scissors'))
+
+updateScore();
+
+
+const endgameModal = document.getElementById('endgameModal');
+const endgameMsg = document.getElementById('endgameMsg');
+const restartBtn = document.getElementById('restartBtn');
+
+function showEndgameModal(message) {
+  endgameMsg.textContent = message;
+  endgameModal.classList.add('active');
+}
+
+function hideEndgameModal() {
+  endgameModal.classList.remove('active');
+}
+
+function restartGame() {
+    gameOver = false;
+    playerScore = 0;
+    computerScore = 0;
+    userChoiceElement.textContent = 'Your Choice: undefined';
+    computerChoiceElement.textContent = "Computer's Choice: undefined";
+    resultsElement.textContent = 'Result: undefined';
+    updateScore();
+    rock.style.display = '';
+    paper.style.display = '';
+    scissors.style.display = '';
+    hideEndgameModal();
+  }
+
+restartBtn.addEventListener('click', restartGame);
+
